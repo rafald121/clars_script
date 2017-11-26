26244f0b635a8a24a5f45e65fb43cd566d1592c4
rafald121 <rafald121@gmail.com>
2017-11-26 15:05:15 +0100

    przed dodaniem podsumowania z danej kategorii

---
+++ script.js
@@ -0,0 +1,437 @@
+COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+COLOR_DOSTAWA = '#C5E89B'
+COLOR_ZWROT = '#57a4f2' 
+COLOR_WPLATA = '#D46A6A'
+
+COLOR_SUMA = '#f99090'
+
+COL_LP = 'A';COL_DATE = 'B';COL_MODEL = 'C';COL_SZTUKI = 'D';COL_CENA_SZT = 'E';COL_SUMA = 'F';INNE = 'G';
+COL_SUMA_WPLATA = 'F'
+
+COLUMNS_INDEX = {
+  COL_LP:0,  COL_DATE:1,  COL_MODEL:2,  COL_SZTUKI:3,  COL_CENA_SZT:4,  COL_SUMA:5,  INNE:6,
+}
+COLUMNS__WPLATA_INDEX = {
+  COL_LP:0,  COL_DATE:1,  COL_MODEL:2,  COL_SZTUKI:3,  COL_CENA_SZT:4,  COL_CENA_SZT_SPRZEDAZ:5,  COL_SUMA:6, INNE:7
+}
+DOSTAWA = 'dostawa';ZWROT = 'zwrot';WPLATA = 'wplata';SUMA = 'suma';DATA = 'Data';
+DOSTAWA_NAME = 'Dostawa' ; ZWROT_NAME = 'Zwrot'; WPLATA_NAME = 'Wplata' ; WSZYSTKO_NAME = 'Wszystko';
+
+LEFT_CELL = 'A'
+RIGHT_CELL = 'G'
+
+var ss = SpreadsheetApp.getActiveSpreadsheet();
+
+var sheetDostawa = ss.getSheets()[0];
+var sheetZwrot = ss.getSheets()[1];
+var sheetWplata = ss.getSheets()[2];
+var sheetSuma = ss.getSheets()[3];
+
+var sheetDostawa1 = ss.getSheetByName(DOSTAWA_NAME)
+var sheetZwrot1 = ss.getSheetByName(ZWROT_NAME)
+var sheetWplata1 = ss.getSheetByName(WPLATA_NAME)
+var sheetSuma1 = ss.getSheetByName(WSZYSTKO_NAME)
+
+sheetDict = {
+  DOSTAWA:sheetDostawa,
+  ZWROT:sheetZwrot,
+  WPLATA:sheetWplata,
+  SUMA:sheetSuma,
+}
+
+
+
+dataList = {
+  DOSTAWA: sheetDostawa.getDataRange().getValues(),
+  ZWROT: sheetZwrot.getDataRange().getValues(),
+  WPLATA: sheetWplata.getDataRange().getValues(),
+  SUMA: sheetSuma.getDataRange().getValues(),
+}
+
+///////////////////////////////// FUNCTION START /////////////////////////////////
+
+function main(){
+  countDostawa()
+  countZwrot()
+  countWplata()
+}
+////////////////////////////////////////////////////
+/////////////   POLICZ TABELE DOSTAWA //////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+function countDostawa() {
+  maxRow = dataList.DOSTAWA.length
+  sumaIndicatorIndexes = getArrayOfSumaIndicators(DOSTAWA)
+  dictFirstLastToSum = getFirstLastRowToSum(sumaIndicatorIndexes)
+  
+  for ( var sumaNr = 1 ; sumaNr<sumaIndicatorIndexes.length ; sumaNr++){ //zsumuj po kolei dla kazdej daty    // sumaNr = 1 bo pomijamy headera
+    row = sumaIndicatorIndexes[sumaNr]
+    row_val = dictFirstLastToSum[row] // first last 
+    countSumaSztuki(row, row_val, DOSTAWA)
+    countSumaSuma(row, row_val, DOSTAWA)
+  }
+}
+////////////////////////////////////////////////////
+/////////////   POLICZ TABELE ZWROT //////////////
+////////////////////////////////////////////////////
+function countZwrot() {
+  maxRow = dataList.ZWROT.length
+  sumaIndicatorIndexes = getArrayOfSumaIndicators(ZWROT)
+  dictFirstLastToSum = getFirstLastRowToSum(sumaIndicatorIndexes)
+  
+  for ( var sumaNr = 1 ; sumaNr<sumaIndicatorIndexes.length ; sumaNr++){ //zsumuj po kolei dla kazdej daty    // sumaNr = 1 bo pomijamy headera
+    row = sumaIndicatorIndexes[sumaNr]
+    row_val = dictFirstLastToSum[row] // first last 
+    countSumaSztuki(row, row_val, ZWROT)
+    countSumaSuma(row, row_val, ZWROT)
+  }
+}
+////////////////////////////////////////////////////
+/////////////   POLICZ TABELE WPLATA ///////////////
+////////////////////////////////////////////////////
+function countWplata(){
+  maxRow = dataList.WPLATA.length // liczba wierszy
+  sumaIndicatorIndexes = getArrayOfSumaIndicators(WPLATA) // indexy wierszy które sumujemy
+  dictFirstLastToSum = getFirstLastRowToSum(sumaIndicatorIndexes) // słownik pierwszego i ostatniego wiersza z kazdej sumy
+  
+  for ( var sumaNr = 1 ; sumaNr<sumaIndicatorIndexes.length ; sumaNr++){ //zsumuj po kolei dla kazdej daty    // sumaNr = 1 bo pomijamy headera
+    row = sumaIndicatorIndexes[sumaNr]  // wiersz z sumą na daną iterację // 6, 12, 18, 23//
+    row_val = dictFirstLastToSum[row] // dla 6: [2,5], dla 11: [7,10] 
+    countSumaSztuki(row, row_val, WPLATA)
+    countSumaSumaWplata(row, row_val, WPLATA)
+  }
+}
+////////////////////////////////////////////////////
+/////////////   POLICZ TABELE WSZYSTKO /////////////
+////////////////////////////////////////////////////
+function countWszystko(){
+  //countDostawa()
+  //countZwrot()
+  //countWplata()
+
+  firstDateDostawa = getFirstDate(DOSTAWA)
+  firstDateZwrot = getFirstDate(ZWROT)
+  firstDateWplata = getFirstDate(WPLATA);
+  firstDate = firstDateDostawa
+  firstDates = [firstDateDostawa, firstDateZwrot, firstDateWplata]
+  
+  lastDateDostawa = getLastDate(DOSTAWA);
+  lastDateZwrot = getLastDate(ZWROT); 
+  lastDateWplata = getLastDate(WPLATA);
+  lastDate = lastDateDostawa
+  lastDates = [lastDateDostawa, lastDateZwrot, lastDateWplata];
+  for(var i = 0 ; i<3 ; i++){
+   if(firstDates[i] < firstDate){
+     firstDate = firstDates[i]
+   }
+   if(lastDates[i] > lastDate){
+     lastDate = lastDates[i]
+   }                   
+ }
+ 
+  maxRowWplata = dataList.WPLATA.length // liczba wierszy
+  sumaIndicatorIndexesWplata = getArrayOfSumaIndicators(WPLATA) // indexy wierszy które sumujemy
+  dictFirstLastToSumWplata = getFirstLastRowToSum(sumaIndicatorIndexesWplata) // słownik pierwszego i ostatniego wiersza z kazdej sumy
+  
+  maxRowZwrot = dataList.ZWROT.length
+  sumaIndicatorIndexesZwrot = getArrayOfSumaIndicators(ZWROT)
+  dictFirstLastToSumZwrot = getFirstLastRowToSum(sumaIndicatorIndexesZwrot)
+  
+  maxRowDostawa = dataList.DOSTAWA.length
+  sumaIndicatorIndexesDostawa = getArrayOfSumaIndicators(DOSTAWA)  // pokazuje liczba wierszy z sumą +1 bo header
+  dictFirstLastToSumDostawa = getFirstLastRowToSum(sumaIndicatorIndexesDostawa)
+  // daty z danej tabeli
+  datesDostawa = fillListWithDatesToConsider(dictFirstLastToSumDostawa, DOSTAWA) 
+  datesZwrot   = fillListWithDatesToConsider(dictFirstLastToSumZwrot, ZWROT)
+  datesWplata  = fillListWithDatesToConsider(dictFirstLastToSumWplata, WPLATA)
+  //
+  datesDictDostawa = fillDictWithDatesToConsider(dictFirstLastToSumDostawa, DOSTAWA) 
+  datesDictZwrot   = fillDictWithDatesToConsider(dictFirstLastToSumZwrot, ZWROT)
+  datesDictWplata  = fillDictWithDatesToConsider(dictFirstLastToSumWplata, WPLATA)
+  
+  tableTypeDataFirstLast = [] //tablica tablic o formie: typ, data, first, last
+  for (var key in datesDictDostawa){
+    detail = [DOSTAWA,key,datesDictDostawa[key][0],datesDictDostawa[key][1]]
+    tableTypeDataFirstLast.push(detail)
+  }
+  for (var key in datesDictZwrot){
+    detail = [ZWROT,key,datesDictZwrot[key][0],datesDictZwrot[key][1]]
+    tableTypeDataFirstLast.push(detail)
+  }
+  for (var key in datesDictWplata){
+    detail = [WPLATA,key,datesDictWplata[key][0],datesDictWplata[key][1]]
+    tableTypeDataFirstLast.push(detail)
+  }  
+  
+  sortedTableTypeDataFirstLast = sortDates(tableTypeDataFirstLast, firstDate)
+  
+  rowCounter = 2 // rowCounter==2, bo row=1 to header
+  counter = 1
+  for(var l = 0 ; l < sortedTableTypeDataFirstLast.length ; l++){
+    last = sortedTableTypeDataFirstLast[l][3];
+    first =  sortedTableTypeDataFirstLast[l][2];
+    rowAmountToAdd = last-first+1
+    unpackAndPaste(sortedTableTypeDataFirstLast[l], rowCounter, counter);//dodaj do większego od jeden niż ostatni zapełniony 
+    rowCounter += rowAmountToAdd;
+    counter+=rowAmountToAdd
+  }
+  
+  var lastRow = counter+1
+  allSumaCell = joinCell('G',lastRow)
+  allSztukiCell = joinCell('E',lastRow)
+  
+  sumaFirst = joinCell('G', 2)
+  sumaLast = joinCell('G', lastRow-1)
+  sztukiFirst = joinCell('E', 2)
+  sztukiLast = joinCell('E', lastRow-1)
+  getSheetType(SUMA).getRange(allSumaCell).setValue(sumFormula(sumaFirst,sumaLast))
+  getSheetType(SUMA).getRange(allSztukiCell).setValue(sumFormula(sztukiFirst, sztukiLast))
+  
+  
+  var a = 1
+  
+}
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+/////////////// UTILS LICZENIE WSZYSTKIEGO /////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+
+function unpackAndPaste(row, rowCounter, counter){//row
+// first/last CellTo - pierwsza/ostatnia komórka do której będziemy doklejać 
+// first/last CellFrom - pierwsza/ostatnia komórka z której będziemy doklejać  
+
+  type = row[0]
+  if(type == DOSTAWA){
+    color=COLOR_DOSTAWA
+  } else if(type == ZWROT){
+    color=COLOR_ZWROT
+  } else if(type == WPLATA){
+    color=COLOR_WPLATA
+  }
+  
+  // KOPIOWANIE
+  counterRow = 0 //liczy ile wierszy wkleja
+  for(var i = row[2] ; i<=row[3] ; i++){
+    firstCellFrom = joinCell(LEFT_CELL, i)
+    lastCellFrom = joinCell(RIGHT_CELL, i)
+    rangeFrom = rangeFormula(firstCellFrom, lastCellFrom)
+    
+    firstCellTo = joinCell(LEFT_CELL, rowCounter+counterRow)
+    lastCellTo = joinCell(RIGHT_CELL, rowCounter+counterRow)
+    rangeTo = rangeFormula(firstCellTo, lastCellTo)
+    
+    lpCell = joinCell('A',counter+1)
+    typeCell = joinCell('B', rowCounter+counterRow)
+    
+    var sourceRowFrom = getSheetType(type).getRange(i, 1, 1, 8)
+    var targetRowTo = getSheetType(SUMA).getRange(rowCounter+counterRow, 2, 1, 8)
+
+    sourceRowFrom.copyTo(targetRowTo)    
+
+//KOLORKI do sumy
+    getSheetType(SUMA).getRange(lpCell).setValue(counter)
+    getSheetType(SUMA).getRange(typeCell).setValue(type)
+    getSheetType(SUMA).getRange(rowCounter+counterRow, 1, 1, 8).setBackground(color);
+    
+    counterRow++
+    counter++
+  }
+
+}
+
+function sortDates(_tableTypeDataFirstLast, _firstDate){
+  tableTypeDataFirstLast = []
+  for (var k = 0 ; k<_tableTypeDataFirstLast.length; k++){
+    tableTypeDataFirstLast.push(_tableTypeDataFirstLast[k])
+  }
+  len = tableTypeDataFirstLast.length
+  var table = []
+  
+  for(var i = 0 ; i < len ; i++){ // pętla do całej tablicy z danymi
+  
+    date = tableTypeDataFirstLast[0][1]
+    earliest_date = [new Date(date), 0]  //data i numer indeksu w oryginale
+    var j = 0
+    for (j = 0 ; j < tableTypeDataFirstLast.length ; j++){
+
+      date = tableTypeDataFirstLast[j][1]
+      currently_checking_date = new Date(date)       
+
+      if (currently_checking_date.getTime() < earliest_date[0].getTime()){
+        earliest_date = [currently_checking_date, j]
+      }
+     
+    }
+    
+    table.push(tableTypeDataFirstLast[earliest_date[1]])
+    
+    tableTypeDataFirstLast.splice(earliest_date[1],1)
+
+    
+  }
+  return table
+}
+
+//lista dat, z każdego typu tabelu (dostawa,zwrto,wplata), którą pozniej uwzgledniamy w tabeli wszystko
+function fillListWithDatesToConsider(dict, sheetType){
+  dateList = []
+  for (var key in dict){
+    targetCell = joinCell(COL_DATE, dict[key][0])
+    date = getSheetType(sheetType).getRange(targetCell).getValue()
+    dateList.push(date)
+  }
+  return dateList //zwraca wszystkie daty 
+}
+// zwraca slownik  Date : [first_row,last_row] <- now we know which row copy to "wszystko" table 
+function fillDictWithDatesToConsider(dict, sheetType){
+  dateDict = {}
+  for (var key in dict){
+    targetCell = joinCell(COL_DATE, dict[key][0])
+    date = getSheetType(sheetType).getRange(targetCell).getValue()
+    dateDict[date] = dict[key]
+  }
+  return dateDict  
+}
+
+function getFirstDate(){
+  
+}
+
+
+//////////////WEZ INDEKSY GRANICZNE OD DO KTORYCH TRZEBA ZSUMOWAC//////////////////////////////////////
+function getFirstLastRowToSum(sumaIndicatorIndexes){
+  var i;
+  dict = {}
+  
+  for (i=1;i<sumaIndicatorIndexes.length;i++){ // nie uwzgledniamy '1' bo to headery
+    first = sumaIndicatorIndexes[i-1]+1 
+    last = sumaIndicatorIndexes[i]-1
+    dict[sumaIndicatorIndexes[i]] = [first,last]
+  }
+  return dict
+}
+/////SUMY,AVGY//////////////////
+
+function setAmountIndex(indexes){
+
+}
+function countSumaSztuki(row, firstLast, sheetType){
+  firstCell = joinCell(COL_SZTUKI,firstLast[0])
+  lastCell = joinCell(COL_SZTUKI,firstLast[1])
+  targetCell = joinCell(COL_SZTUKI, row)
+  getSheetType(sheetType).getRange(targetCell).setValue(sumFormula(firstCell,lastCell));
+  getSheetType(sheetType).getRange(targetCell).setBackground(COLOR_SUMA)  
+}
+function countSumaSuma(row, firstLast, sheetType){
+  firstCell = joinCell(COL_SUMA,firstLast[0])
+  lastCell = joinCell(COL_SUMA,firstLast[1])
+  targetCell = joinCell(COL_SUMA, row)
+  getSheetType(sheetType).getRange(targetCell).setValue(sumFormula(firstCell,lastCell));
+  getSheetType(sheetType).getRange(targetCell).setBackground(COLOR_SUMA);
+}
+function countSumaSumaWplata(row, firstLast, sheetType){
+  firstCell = joinCell(COL_SUMA_WPLATA,firstLast[0])
+  lastCell = joinCell(COL_SUMA_WPLATA,firstLast[1])
+  targetCell = joinCell(COL_SUMA_WPLATA, row)
+  getSheetType(sheetType).getRange(targetCell).setValue(sumFormula(firstCell,lastCell));
+  getSheetType(sheetType).getRange(targetCell).setBackground(COLOR_SUMA);
+}
+
+function countAvgCenaSzt(indexes){ 
+}
+
+/////NUMERY WIERSZY W KTORYCH SUMUJEMY//////////////////
+
+function getArrayOfSumaIndicators(sheetType) {//sheetType = DOSTAWA,WPLATA,...
+  data = getDataType(sheetType)
+  var row;
+  sumaIndicateIndexes = [];
+  for(row=0;row<data.length; row++){
+    var varsuma = data[row][COLUMNS_INDEX.COL_DATE];
+    if(varsuma===SUMA || varsuma===DATA)  
+      sumaIndicateIndexes.push(row+1);//+1 bo spreadsheet ma indexy od 1, a nie od 0
+  }
+  return sumaIndicateIndexes;
+}
+
+////UTILS ////////////////////////////////////////////
+
+function getFirstDate(sheetType){
+  return getSheetType(sheetType).getRange('B2').getValue();
+}
+
+function getLastDate(sheetType){
+  data = getDataType(sheetType)
+  lastDateCell = joinCell('B', data.length-2)
+  return getSheetType(sheetType).getRange(lastDateCell).getValue();
+}
+
+function fillArray(from,to){
+  arraj = []
+  for(i=from;i<to+1;i++)
+    arraj.push(i);
+  return arraj;
+}
+
+function sumFormula(first,last){
+  return "=SUM("+first+":"+last+")";
+}
+
+function rangeFormula(first, last){
+  return first+":"+last
+}
+
+function joinCell(col,row){ // np 6 + F = F6
+  return col + row.toString()
+}
+
+///GETTERY /////////////////////////////////////////
+
+function getSheetType(sheetType){
+  data = null;
+  if (sheetType == DOSTAWA)
+    data = sheetDict.DOSTAWA
+  else if(sheetType == ZWROT)
+    data = sheetDict.ZWROT
+  else if(sheetType == WPLATA)
+    data = sheetDict.WPLATA
+  else if(sheetType == SUMA)
+    data = sheetDict.SUMA
+  else
+    data = 'blad'
+    
+   return data
+}
+
+function getDataType(sheetType){
+  data = null
+  if (sheetType == DOSTAWA)
+    data = dataList.DOSTAWA
+  else if(sheetType == ZWROT)
+    data = dataList.ZWROT
+  else if(sheetType == WPLATA)
+    data = dataList.WPLATA
+  else if(sheetType == SUMA)
+    data = dataList.SUMA
+  else
+    data = 'blad'
+    
+   return data
+}
